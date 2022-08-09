23ab284c0691be74a93d98d1190d9867eddab18c
David López <d.lopez@saltosystems.com>
2022-08-09 11:52:47 +0200

    *: set the led and measure the moisture in grove sensor scale

--- tinyplant.go
+++ tinyplant.go
@@ -6,29 +6,36 @@ import (
 )
 
 func main() {
+	// Configure the integrated led.
+	led := machine.LED
+	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
+	// Configure the ADC0 pin on the Arduino board.
 	machine.InitADC()
-
-	// TODO(lopezator): No led for now
-	// led := machine.LED
-	// led.Configure(machine.PinConfig{Mode: machine.PinOutput})
-
-	// Configure the ADC0 pin is the analog pin on the Arduino board.
 	sensor := machine.ADC{Pin: machine.ADC0}
 	sensor.Configure(machine.ADCConfig{})
 
-	for {
-		val := sensor.Get()
-		/*
-			    Official values of "Grove Moisture Sensor" in a 0-1024 scale:
+	/*
+		Official values of "Grove Moisture Sensor" in a 0-1024 scale:
 
-				Output Value           Min     Max
-				Sensor in dry soil       0     300
-				Sensor in humid soil   300     700
-				Sensor in water        700     950
-		*/
-		// TODO(lopezator): Why this operation doesn't work and always return zero?
-		// val = val * 1024 / 65535
-		println("The value of the moisture sensor is: ", val)
-		time.Sleep(time.Millisecond * 500)
+		Output Value           Min     Max
+		-------------------   -----   -----
+		Sensor in dry soil       0     300
+		Sensor in humid soil   300     700
+		Sensor in water        700     950
+	*/
+	for {
+		// Divide by 64 to "convert" from tinygo adc scale (0xffff, 65535) to moisture sensor scale (0-1024).
+		moisture := sensor.Get() / 64
+		println("The value of the moisture sensor is: ", moisture)
+		// If the moisture sensor returns a value equal or under 300, we consider it dry.
+		// We will turn on the LED to indicate this, and keep that state until the plant is watered.
+		if moisture <= 300 {
+			led.High()
+		} else {
+			led.Low()
+		}
+		// Wait a second to read the sensor value again.
+		time.Sleep(1 * time.Second)
 	}
 }
